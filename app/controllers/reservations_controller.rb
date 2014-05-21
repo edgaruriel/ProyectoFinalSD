8c5913c63759c10a8e6341046ebfeb0b41c299bd
jaime negrete <josejaimenegrete@hotmail.com>
2014-05-21 13:40:21 -0500

    comentarios

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,14 @@
 class ReservationsController < ApplicationController
   # before_filter :authenticate_user!
+  #obtenemos todos los asientos de la base de datos y los mandamos a la vista
   def new
     @seats = Seat.all
   end
 
+  #obtiene las reservaciones de un numero de reservacion
   def show
     @reservations = Reservation.where(number: params[:id])
+    #si el numero de reservacion no contiene reservaciones lo redirige al seat chooser
     if @reservations.count == 0
       redirect_to edit_seat_path
     end
@@ -14,12 +17,13 @@ class ReservationsController < ApplicationController
   def create
     @reservations = Reservation.where(ip: @ip)
     @reservations.each do |reservation|
-      reservation.number = @reservations.first.id
+      reservation.number = @reservations.first.id #obtenemos el numero de reservacion
       reservation.confirm_reservation
     end
     redirect_to reservation_path(:id => @reservations.first.number)
   end
 
+  #destruye el asiento de una reservacion
   def destroy
     reservation = Reservation.find(params[:id])
     # @reservations.each do |reservation|
@@ -28,6 +32,7 @@ class ReservationsController < ApplicationController
       reservation.seat.save
       reservation.destroy
     # end
+    #Actualiza la vista para dejar de mostrar el asiento destruido
     redirect_to reservation_path(id: reservation.number)
   end
 
--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,5 +1,6 @@
 class SeatsController < ApplicationController
   # enable_sync only: [:create, :edit, :update, :destroy]
+
   def index
     @seats = Seat.all
   end
@@ -8,6 +9,7 @@ class SeatsController < ApplicationController
     @seats = Seat.all
     sum_time(3)
   end
+
 
   def update
     # debugger
@@ -22,6 +24,7 @@ class SeatsController < ApplicationController
     end
     valid = true
 
+    #si se selecciona un asiento y ya se tenian seleccionados 5, muestra una alerta
     if(Reservation.where(ip: @ip).count.to_i >= 5) && params[:seat][:prereserved] != '0'
       valid = false
       respond_to do |format|
@@ -29,6 +32,7 @@ class SeatsController < ApplicationController
         # redirect_to edit_seat_path(@seat)
       end
     end
+    #si se selecciona un asiento preservado, mostramos una alerta
     if params[:seat][:prereserved] == '2' || seat_prereserved == params[:seat][:prereserved]
       valid = false
       # el 2 es cuando ya estaba prereservado el asiento.
@@ -37,6 +41,7 @@ class SeatsController < ApplicationController
         # redirect_to edit_seat_path(@seat)
       end
 
+    #si el asiento seleccionado no ha sido preservado y ha sido actualizado en la BD
     elsif valid && @seat.update(seat_params)
       if !@seat.reserved && @seat.prereserved
         reservation = Reservation.create(:seat_id => @seat.id, :ip => @ip)
@@ -56,11 +61,12 @@ class SeatsController < ApplicationController
     # end
   end
 
-private
+  private
   def seat_params
     params.require(:seat).permit(:user_id, :reserved, :prereserved, :number, :row)
   end
 
+  # actualiza el contador de tiempo y resetea la vista si la sesion dura mas de 60 segundos
   def sum_time(time)
     @ip_object = Ip.find_by_ip(@ip)
     if @ip_object.nil?
